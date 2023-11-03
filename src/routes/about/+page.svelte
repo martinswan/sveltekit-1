9f07806dfcb8010bab2d16192f9ffad06b752832
martinswan <36090745+martinswan@users.noreply.github.com>
2023-11-03 14:55:44 -0500

    Update +page.svelte
    Oops. Let's undo that!

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -21,6 +21,6 @@
 
 	<p>
 		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled! test update
+		using it with JavaScript disabled!
 	</p>
 </div>
